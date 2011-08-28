be0f26beec42e44652143c6b488fd6176615ca6d
Marcus Westin <marcus.westin@gmail.com>
2011-08-27 19:23:14 -0700

    Remove filed headers

--- WebViewJavascriptBridge/ExampleWebViewJavascriptBridgeDelegate.h
+++ WebViewJavascriptBridge/ExampleWebViewJavascriptBridgeDelegate.h
@@ -1,11 +1,3 @@
-//
-//  ExampleWebViewJavascriptBridgeDelegate.h
-//  WebViewJavascriptBridge
-//
-//  Created by John Marcus Westin on 8/27/11.
-//  Copyright (c) 2011 Clover. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import "WebViewJavascriptBridge.h"
 
--- WebViewJavascriptBridge/ExampleWebViewJavascriptBridgeDelegate.m
+++ WebViewJavascriptBridge/ExampleWebViewJavascriptBridgeDelegate.m
@@ -1,11 +1,3 @@
-//
-//  ExampleWebViewJavascriptBridgeDelegate.m
-//  WebViewJavascriptBridge
-//
-//  Created by John Marcus Westin on 8/27/11.
-//  Copyright (c) 2011 Clover. All rights reserved.
-//
-
 #import "ExampleWebViewJavascriptBridgeDelegate.h"
 
 @implementation ExampleWebViewJavascriptBridgeDelegate
