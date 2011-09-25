604bf4cbb5f50998a2ffe3451f78f0bf8f04f44a
Marcus Westin <marcus.westin@gmail.com>
2011-09-25 14:16:10 -0700

    Remove file header

--- WebViewJavascriptBridge/main.m
+++ WebViewJavascriptBridge/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  WebViewJavascriptBridge
-//
-//  Created by John Marcus Westin on 8/27/11.
-//  Copyright (c) 2011 Clover. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
