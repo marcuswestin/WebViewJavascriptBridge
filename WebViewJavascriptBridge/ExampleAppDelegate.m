96eedc6cf735f9827da1b9ac198b66c4511a886b
Marcus Westin <marcus.westin@gmail.com>
2011-12-04 12:15:38 -0800

    Clicking the JS button in the demo felt very slow. Send the message right away ontouchstart instead. Not the correct UX, but I want to avoid the impression that passing messages is slow

--- WebViewJavascriptBridge/ExampleAppDelegate.m
+++ WebViewJavascriptBridge/ExampleAppDelegate.m
@@ -57,7 +57,7 @@
          "      WebViewJavascriptBridge.sendMessage('hello from the JS');"
          "      var button = document.body.appendChild(document.createElement('button'));"
          "      button.innerHTML = 'Click me to send a message to ObjC';"
-         "      button.onclick = function() { WebViewJavascriptBridge.sendMessage('hello from JS button'); };"
+         "      button.onclick = button.ontouchstart = function() { WebViewJavascriptBridge.sendMessage('hello from JS button'); };"
          "  }"
          "  </script>"
          "</body></html>" baseURL:nil];
