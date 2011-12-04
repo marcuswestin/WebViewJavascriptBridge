d53c8d28733e8a55f8c690a8a56bb01133caeac2
Marcus Westin <marcus.westin@gmail.com>
2011-12-04 12:13:15 -0800

    Whitespace fixes for style consistency. No functional changes

--- WebViewJavascriptBridge/Classes/WebViewJavascriptBridge.m
+++ WebViewJavascriptBridge/Classes/WebViewJavascriptBridge.m
@@ -18,37 +18,31 @@ static NSString *MESSAGE_SEPARATOR = @"__wvjb_sep__";
 static NSString *CUSTOM_PROTOCOL_SCHEME = @"webviewjavascriptbridge";
 static NSString *QUEUE_HAS_MESSAGE = @"queuehasmessage";
 
-- (id)initWithDelegate:(id <WebViewJavascriptBridgeDelegate>)delegate
-{
-    if ((self = [super init]) )
-    {
-		self.delegate = delegate;
+- (id)initWithDelegate:(id <WebViewJavascriptBridgeDelegate>)delegate {
+    if ((self = [super init])) {
+        self.delegate = delegate;
         self.startupMessageQueue = [[[NSMutableArray alloc] init] autorelease];
     }
-    
+
     return self;
 }
 
-- (id)init
-{
+- (id)init {
     return [[[WebViewJavascriptBridge alloc] initWithDelegate:nil] autorelease];
 }
 
-+ (id)javascriptBridgeWithDelegate:(id <WebViewJavascriptBridgeDelegate>)delegate
-{
++ (id)javascriptBridgeWithDelegate:(id <WebViewJavascriptBridgeDelegate>)delegate {
     return [[[self alloc] initWithDelegate:delegate] autorelease];
 }
 
-+ (id)javascriptBridge
-{
++ (id)javascriptBridge {
     return [[[self alloc] init] autorelease];
 }
 
-- (void)dealloc
-{
+- (void)dealloc {
     _delegate = nil;
     [_startupMessageQueue release];
-    
+
     [super dealloc];
 }
 
@@ -58,9 +52,9 @@ static NSString *QUEUE_HAS_MESSAGE = @"queuehasmessage";
 }
 
 - (void)_doSendMessage:(NSString *)message toWebView:(UIWebView *)webView {
-	message = [message stringByReplacingOccurrencesOfString:@"\\n" withString:@"\\\\n"];
-	message = [message stringByReplacingOccurrencesOfString:@"'" withString:@"\\'"];
-	message = [message stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
+    message = [message stringByReplacingOccurrencesOfString:@"\\n" withString:@"\\\\n"];
+    message = [message stringByReplacingOccurrencesOfString:@"'" withString:@"\\'"];
+    message = [message stringByReplacingOccurrencesOfString:@"\"" withString:@"\\\""];
     [webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"WebViewJavascriptBridge._handleMessageFromObjC('%@');", message]];
 }
 
@@ -75,115 +69,107 @@ static NSString *QUEUE_HAS_MESSAGE = @"queuehasmessage";
 #pragma mark UIWebViewDelegate
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
-	NSString *js = [NSString stringWithFormat:@";(function() {"
-					"if (window.WebViewJavascriptBridge) { return; };"
-					"var _readyMessageIframe,"
-					"     _sendMessageQueue = [],"
-					"     _receiveMessageQueue = [],"
-					"     _MESSAGE_SEPERATOR = '%@',"
-					"     _CUSTOM_PROTOCOL_SCHEME = '%@',"
-					"     _QUEUE_HAS_MESSAGE = '%@';"
-					""
-					"function _createQueueReadyIframe(doc) {"
-					"     _readyMessageIframe = doc.createElement('iframe');"
-					"     _readyMessageIframe.style.display = 'none';"
-					"     doc.documentElement.appendChild(_readyMessageIframe);"
-					"}"
-					""
-					"function _sendMessage(message) {"
-					"     _sendMessageQueue.push(message);"
-					"     _readyMessageIframe.src = _CUSTOM_PROTOCOL_SCHEME + '://' + _QUEUE_HAS_MESSAGE;"
-					"};"
-					""
-					"function _fetchQueue() {"
-					"     var messageQueueString = _sendMessageQueue.join(_MESSAGE_SEPERATOR);"
-					"     _sendMessageQueue = [];"
-					"     return messageQueueString;"
-					"};"
-					""
-					"function _setMessageHandler(messageHandler) {"
-					"     if (WebViewJavascriptBridge._messageHandler) { return alert('WebViewJavascriptBridge.setMessageHandler called twice'); }"
-					"     WebViewJavascriptBridge._messageHandler = messageHandler;"
-					"     var receivedMessages = _receiveMessageQueue;"
-					"     _receiveMessageQueue = null;"
-					"     for (var i=0; i<receivedMessages.length; i++) {"
-					"         messageHandler(receivedMessages[i]);"
-					"     }"
-					"};"
-					""
-					"function _handleMessageFromObjC(message) {"
-					"     if (_receiveMessageQueue) { _receiveMessageQueue.push(message); }"
-					"     else { WebViewJavascriptBridge._messageHandler(message); }"
-					"};"
-					""
-					"window.WebViewJavascriptBridge = {"
-					"     setMessageHandler: _setMessageHandler,"
-					"     sendMessage: _sendMessage,"
-					"     _fetchQueue: _fetchQueue,"
-					"     _handleMessageFromObjC: _handleMessageFromObjC"
-					"};"
-					""
-					"setTimeout(function() {"
-					"     var doc = document;"
-					"     _createQueueReadyIframe(doc);"
-					"     var readyEvent = doc.createEvent('Events');"
-					"     readyEvent.initEvent('WebViewJavascriptBridgeReady');"
-					"     doc.dispatchEvent(readyEvent);"
-					"}, 0);"
-					"})();",
-					MESSAGE_SEPARATOR,
-					CUSTOM_PROTOCOL_SCHEME,
-					QUEUE_HAS_MESSAGE];
+    NSString *js = [NSString stringWithFormat:@";(function() {"
+        "if (window.WebViewJavascriptBridge) { return; };"
+        "var _readyMessageIframe,"
+        "     _sendMessageQueue = [],"
+        "     _receiveMessageQueue = [],"
+        "     _MESSAGE_SEPERATOR = '%@',"
+        "     _CUSTOM_PROTOCOL_SCHEME = '%@',"
+        "     _QUEUE_HAS_MESSAGE = '%@';"
+        ""
+        "function _createQueueReadyIframe(doc) {"
+        "     _readyMessageIframe = doc.createElement('iframe');"
+        "     _readyMessageIframe.style.display = 'none';"
+        "     doc.documentElement.appendChild(_readyMessageIframe);"
+        "}"
+        ""
+        "function _sendMessage(message) {"
+        "     _sendMessageQueue.push(message);"
+        "     _readyMessageIframe.src = _CUSTOM_PROTOCOL_SCHEME + '://' + _QUEUE_HAS_MESSAGE;"
+        "};"
+        ""
+        "function _fetchQueue() {"
+        "     var messageQueueString = _sendMessageQueue.join(_MESSAGE_SEPERATOR);"
+        "     _sendMessageQueue = [];"
+        "     return messageQueueString;"
+        "};"
+        ""
+        "function _setMessageHandler(messageHandler) {"
+        "     if (WebViewJavascriptBridge._messageHandler) { return alert('WebViewJavascriptBridge.setMessageHandler called twice'); }"
+        "     WebViewJavascriptBridge._messageHandler = messageHandler;"
+        "     var receivedMessages = _receiveMessageQueue;"
+        "     _receiveMessageQueue = null;"
+        "     for (var i=0; i<receivedMessages.length; i++) {"
+        "         messageHandler(receivedMessages[i]);"
+        "     }"
+        "};"
+        ""
+        "function _handleMessageFromObjC(message) {"
+        "     if (_receiveMessageQueue) { _receiveMessageQueue.push(message); }"
+        "     else { WebViewJavascriptBridge._messageHandler(message); }"
+        "};"
+        ""
+        "window.WebViewJavascriptBridge = {"
+        "     setMessageHandler: _setMessageHandler,"
+        "     sendMessage: _sendMessage,"
+        "     _fetchQueue: _fetchQueue,"
+        "     _handleMessageFromObjC: _handleMessageFromObjC"
+        "};"
+        ""
+        "setTimeout(function() {"
+        "     var doc = document;"
+        "     _createQueueReadyIframe(doc);"
+        "     var readyEvent = doc.createEvent('Events');"
+        "     readyEvent.initEvent('WebViewJavascriptBridgeReady');"
+        "     doc.dispatchEvent(readyEvent);"
+        "}, 0);"
+        "})();",
+        MESSAGE_SEPARATOR,
+        CUSTOM_PROTOCOL_SCHEME,
+        QUEUE_HAS_MESSAGE];
     
     [webView stringByEvaluatingJavaScriptFromString:js];
     
     for (id message in self.startupMessageQueue) {
         [self _doSendMessage:message toWebView: webView];
     }
-	
+
     self.startupMessageQueue = nil;
-	
-	if(self.delegate != nil && [self.delegate respondsToSelector:@selector(webViewDidFinishLoad:)])
-	{
-		[self.delegate webViewDidFinishLoad:webView];
-	}
+
+    if(self.delegate != nil && [self.delegate respondsToSelector:@selector(webViewDidFinishLoad:)]) {
+        [self.delegate webViewDidFinishLoad:webView];
+    }
 }
 
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
-{
-	if(self.delegate != nil && [self.delegate respondsToSelector:@selector(webView:didFailLoadWithError:)])
-	{
-		[self.delegate webView:webView didFailLoadWithError:error];
-	}
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    if(self.delegate != nil && [self.delegate respondsToSelector:@selector(webView:didFailLoadWithError:)]) {
+        [self.delegate webView:webView didFailLoadWithError:error];
+    }
 }
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     NSURL *url = [request URL];
-    if (![[url scheme] isEqualToString:CUSTOM_PROTOCOL_SCHEME])
-	{
-		if(self.delegate != nil && [self.delegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)])
-		{
-			[self.delegate webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
-		}
-		return YES;
-	}
-	
+    if (![[url scheme] isEqualToString:CUSTOM_PROTOCOL_SCHEME]) {
+        if (self.delegate != nil && [self.delegate respondsToSelector:@selector(webView:shouldStartLoadWithRequest:navigationType:)]) {
+            [self.delegate webView:webView shouldStartLoadWithRequest:request navigationType:navigationType];
+        }
+        return YES;
+    }
+
     if ([[url host] isEqualToString:QUEUE_HAS_MESSAGE]) {
         [self _flushMessageQueueFromWebView: webView];
-    } 
-	else {
+    } else {
         NSLog(@"WebViewJavascriptBridge: WARNING: Received unknown WebViewJavascriptBridge command %@://%@", CUSTOM_PROTOCOL_SCHEME, [url path]);
     }
-    
+
     return NO;
 }
 
-- (void)webViewDidStartLoad:(UIWebView *)webView
-{
-	if(self.delegate != nil && [self.delegate respondsToSelector:@selector(webViewDidStartLoad:)])
-	{
-		[self.delegate webViewDidStartLoad:webView];
-	}
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    if(self.delegate != nil && [self.delegate respondsToSelector:@selector(webViewDidStartLoad:)]) {
+        [self.delegate webViewDidStartLoad:webView];
+    }
 }
 
 @end
--- WebViewJavascriptBridge/ExampleAppDelegate.m
+++ WebViewJavascriptBridge/ExampleAppDelegate.m
@@ -6,8 +6,7 @@
 @synthesize webView = _webView;
 @synthesize javascriptBridge = _javascriptBridge;
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
     self.webView = [[UIWebView alloc] initWithFrame:self.window.bounds];
@@ -15,89 +14,82 @@
 
     self.javascriptBridge = [WebViewJavascriptBridge javascriptBridgeWithDelegate:self];
     self.webView.delegate = self.javascriptBridge;
-	
-	UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-	[button setTitle:@"Send message" forState:UIControlStateNormal];
-	[button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
-	[self.window insertSubview:button aboveSubview:self.webView];
-	button.frame = CGRectMake(90, 400, 130, 45);
-	
-	[self.javascriptBridge sendMessage:@"Message from ObjC before Webview is complete!" toWebView:self.webView];
-    
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [button setTitle:@"Send message" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.window insertSubview:button aboveSubview:self.webView];
+    button.frame = CGRectMake(90, 400, 130, 45);
+
+    [self.javascriptBridge sendMessage:@"Message from ObjC before Webview is complete!" toWebView:self.webView];
+
     [self loadExamplePage];
-    
+
     [self.javascriptBridge sendMessage:@"Message 2 from ObjC before Webview is complete!" toWebView:self.webView];
 
-	[self.window makeKeyAndVisible];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)buttonPressed:(id)sender
-{
-	[self.javascriptBridge sendMessage:@"Message from ObjC on normal situations!" toWebView:self.webView];
+- (void)buttonPressed:(id)sender {
+    [self.javascriptBridge sendMessage:@"Message from ObjC on normal situations!" toWebView:self.webView];
 }
 
-- (void)javascriptBridge:(WebViewJavascriptBridge *)bridge receivedMessage:(NSString *)message fromWebView:(UIWebView *)webView
-{
-	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Message from Javascript" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-	[alert show];
+- (void)javascriptBridge:(WebViewJavascriptBridge *)bridge receivedMessage:(NSString *)message fromWebView:(UIWebView *)webView {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Message from Javascript" message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
 }
 
 - (void)loadExamplePage {
     [self.webView loadHTMLString:@""
-     "<!doctype html>"
-     "<html><head>"
-     "  <style type='text/css'>h1 { color:red; }</style>"
-     "</head><body>"
-     "  <h1>Javascript Bridge Demo</h1>"
-     "  <script>"
-     "  document.addEventListener('WebViewJavascriptBridgeReady', onBridgeReady, false);"
-     "  function onBridgeReady() {"
-     "      WebViewJavascriptBridge.setMessageHandler(function(message) {"
-     "          var el = document.body.appendChild(document.createElement('div'));"
-     "          el.innerHTML = message;"
-     "      });"
-     "      WebViewJavascriptBridge.sendMessage('hello from the JS');"
-     "      var button = document.body.appendChild(document.createElement('button'));"
-     "      button.innerHTML = 'Click me to send a message to ObjC';"
-     "      button.onclick = function() { WebViewJavascriptBridge.sendMessage('hello from JS button'); };"
-     "  }"
-     "  </script>"
-     "</body></html>" baseURL:nil];
+         "<!doctype html>"
+         "<html><head>"
+         "  <style type='text/css'>h1 { color:red; }</style>"
+         "</head><body>"
+         "  <h1>Javascript Bridge Demo</h1>"
+         "  <script>"
+         "  document.addEventListener('WebViewJavascriptBridgeReady', onBridgeReady, false);"
+         "  function onBridgeReady() {"
+         "      WebViewJavascriptBridge.setMessageHandler(function(message) {"
+         "          var el = document.body.appendChild(document.createElement('div'));"
+         "          el.innerHTML = message;"
+         "      });"
+         "      WebViewJavascriptBridge.sendMessage('hello from the JS');"
+         "      var button = document.body.appendChild(document.createElement('button'));"
+         "      button.innerHTML = 'Click me to send a message to ObjC';"
+         "      button.onclick = function() { WebViewJavascriptBridge.sendMessage('hello from JS button'); };"
+         "  }"
+         "  </script>"
+         "</body></html>" baseURL:nil];
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{    
+- (void)applicationWillResignActive:(UIApplication *)application {
     /*
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     /*
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     /*
      Called when the application is about to terminate.
      Save data if appropriate.
