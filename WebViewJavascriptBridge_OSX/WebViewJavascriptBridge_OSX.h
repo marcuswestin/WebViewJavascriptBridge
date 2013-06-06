e563c4a3f67d95c4fb8849c42515c09e6a09496b
Peyton Randolph <peyton@tumblr.com>
2013-06-06 15:01:10 -0400

    add OS X support

--- WebViewJavascriptBridge_OSX/WebViewJavascriptBridge_OSX.h
+++ WebViewJavascriptBridge_OSX/WebViewJavascriptBridge_OSX.h
@@ -6,7 +6,7 @@
 @property (nonatomic, strong) WebView *webView;
 @property (nonatomic, strong) id webViewDelegate;
 
-+ (id)bridgeForWebView:(WebView*)webView handler:(WVJBHandler)handler;
-+ (id)bridgeForWebView:(WebView*)webView webViewDelegate:(id)webViewDelegate handler:(WVJBHandler)handler;
++ (instancetype)bridgeForWebView:(WebView*)webView handler:(WVJBHandler)handler;
++ (instancetype)bridgeForWebView:(WebView*)webView webViewDelegate:(id)webViewDelegate handler:(WVJBHandler)handler;
 
 @end
