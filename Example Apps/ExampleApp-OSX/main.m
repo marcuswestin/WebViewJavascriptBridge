b85f3ff273be12aec831808be7c681206493ea78
Marcus Westin <marcus.westin@gmail.com>
2013-06-08 19:58:03 -0700

    Create an OS X example app that creates everything in code

---
+++ Example Apps/ExampleApp-OSX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ExampleApp-OSX
+//
+//  Created by Marcus Westin on 6/8/13.
+//  Copyright (c) 2013 Marcus Westin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
