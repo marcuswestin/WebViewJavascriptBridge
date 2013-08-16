e05a81b20926d545bcc5e0928a73cc446db373a9
Fang Jian <fangjianmail@126.com>
2013-08-16 12:48:24 +0800

    simple project ,could debug js

--- Android/Example/src/com/example/WebViewJavascriptBridgeExample/MyActivity.java
+++ Android/Example/src/com/example/WebViewJavascriptBridgeExample/MyActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import com.fangjian.WebViewJavascriptBridge;
 
+import java.io.InputStream;
+
 public class MyActivity extends Activity {
+    private WebView webView;
+
     /**
      * Called when the activity is first created.
      */
@@ -13,7 +17,14 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        WebView webView=(WebView) this.findViewById(R.id.webView);
-        webView.addJavascriptInterface(new WebViewJavascriptBridge(),"WebViewJavascriptBridge");
+        webView=(WebView) this.findViewById(R.id.webView);
+        WebViewJavascriptBridge bridge=new WebViewJavascriptBridge(this.getApplicationContext(),webView) ;
+        loadSample();
+    }
+
+    private void loadSample(){
+        InputStream is=getResources().openRawResource(R.raw.sample);
+        String sample=WebViewJavascriptBridge.convertStreamToString(is);
+        webView.loadData(sample,"text/html","UTF-8");
     }
 }
--- Android/Example/src/com/fangjian/WebViewJavascriptBridge.java
+++ Android/Example/src/com/fangjian/WebViewJavascriptBridge.java
@@ -1,5 +1,12 @@
 package com.fangjian;
-import android.webkit.JavascriptInterface;
+import android.content.Context;
+import android.util.Log;
+import android.webkit.*;
+import android.widget.Toast;
+import com.example.WebViewJavascriptBridgeExample.R;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,11 +16,72 @@ import android.webkit.JavascriptInterface;
  */
 public class WebViewJavascriptBridge {
 
-    public WebViewJavascriptBridge() {
+    WebView mWebView;
+    Context mContext;
+    public WebViewJavascriptBridge(Context context,WebView webview) {
+        this.mContext=context;
+        this.mWebView=webview;
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(this, "WebViewJavascriptBridge");
+        mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.setWebChromeClient(new MyWebChromeClient());     //optional, for show console and alert
     }
 
     @JavascriptInterface
-    public void callJava(int callbackId,String message){
-         System.out.println(message+"\n callbackId:"+callbackId);
+    public void callJava(Integer callbackId,String message){
+         Log.i("test", message + " callbackId:" + callbackId);  //id为0，等同null
+        Log.i("test", "message is null?"+(message== null) ) ;
+        Log.i("test", "id is null?"+(callbackId == null) ) ;
+    }
+
+
+    /** Show a toast from the web page */
+    @JavascriptInterface
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadWebViewJavascriptBridgeJs(WebView webView) {
+        InputStream is=mContext.getResources().openRawResource(R.raw.webviewjavascriptbridge);
+        String script=convertStreamToString(is);
+        webView.loadUrl("javascript:"+script);
+
+    }
+
+    public static String convertStreamToString(java.io.InputStream is) {
+        String s="";
+        try{
+            Scanner scanner = new Scanner(is, "UTF-8").useDelimiter("\\A");
+            if (scanner.hasNext()) s= scanner.next();
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView webView, String url) {
+            Log.d("test","onPageFinished");
+            loadWebViewJavascriptBridgeJs(webView);
+        }
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public boolean onConsoleMessage(ConsoleMessage cm) {
+            Log.d("test", cm.message() + " -- From line "
+                    + cm.lineNumber() + " of "
+                    + cm.sourceId() );
+            return true;
+        }
+
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            showToast(message);
+            return true;
+        }
     }
 }
