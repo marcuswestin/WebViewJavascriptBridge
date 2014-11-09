a7c4a038279d57fe5342bb8551571428c310bbbe
lokimeyburg <loki@aristolabs.com>
2014-11-09 14:15:11 -0800

    WKWebView example only available on iOS 8 devices

--- Example Apps/ExampleApp-iOS/ExampleAppDelegate.m
+++ Example Apps/ExampleApp-iOS/ExampleAppDelegate.m
@@ -6,18 +6,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    // 1. Create the UIWebView example
     ExampleUIWebViewController* UIWebViewExampleController = [[ExampleUIWebViewController alloc] init];
     UIWebViewExampleController.tabBarItem.title             = @"UIWebView";
-    
-    ExampleWKWebViewController* WKWebViewExampleController = [[ExampleWKWebViewController alloc] init];
-    WKWebViewExampleController.tabBarItem.title             = @"WKWebView";
-    
+
+    // 2. Create the tab footer and add the UIWebView example
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
     [tabBarController addChildViewController:UIWebViewExampleController];
-    [tabBarController addChildViewController:WKWebViewExampleController];
+    
+    // 3. Create the  WKWebView example for devices >= iOS 8
+    if([WKWebView class]) {
+        ExampleWKWebViewController* WKWebViewExampleController = [[ExampleWKWebViewController alloc] init];
+        WKWebViewExampleController.tabBarItem.title             = @"WKWebView";
+        [tabBarController addChildViewController:WKWebViewExampleController];
+    }
 
-    
-    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = tabBarController;
     [self.window makeKeyAndVisible];
