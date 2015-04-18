86feab4adc5f426367742e212b228bb7343b256b
Pieter De Baets <pieter.debaets@gmail.com>
2015-04-18 15:35:46 +0100

    Add TestHost and tests skeleton

---
+++ WebViewJavascriptBridgeTestHost/AppDelegate.m
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.m
+//  WebViewJavascriptBridgeTestHost
+//
+//  Created by Pieter De Baets on 18/04/2015.
+//  Copyright (c) 2015 marcuswestin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  self.window.rootViewController = [UIViewController new];
+  [self.window makeKeyAndVisible];
+
+  return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ WebViewJavascriptBridgeTests/BridgeTests.m
@@ -0,0 +1,22 @@
+//
+//  BridgeTests.m
+//  WebViewJavascriptBridge
+//
+//  Created by Pieter De Baets on 18/04/2015.
+//  Copyright (c) 2015 marcuswestin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BridgeTests : XCTestCase
+
+@end
+
+@implementation BridgeTests
+
+- (void)testInit
+{
+  XCTAssert(YES);
+}
+
+@end
