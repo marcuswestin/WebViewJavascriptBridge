f99c987292f413d1ce4da134e164a6433bc9d3ea
Marcus Westin <marcus.westin@gmail.com>
2017-01-10 16:51:38 -0500

    Implement removeHandler for WKWebViewJavascriptBridge.
    Fix #260

--- WebViewJavascriptBridge/WKWebViewJavascriptBridge.h
+++ WebViewJavascriptBridge/WKWebViewJavascriptBridge.h
@@ -21,6 +21,7 @@
 + (void)enableLogging;
 
 - (void)registerHandler:(NSString*)handlerName handler:(WVJBHandler)handler;
+- (void)removeHandler:(NSString*)handlerName;
 - (void)callHandler:(NSString*)handlerName;
 - (void)callHandler:(NSString*)handlerName data:(id)data;
 - (void)callHandler:(NSString*)handlerName data:(id)data responseCallback:(WVJBResponseCallback)responseCallback;
--- WebViewJavascriptBridge/WKWebViewJavascriptBridge.m
+++ WebViewJavascriptBridge/WKWebViewJavascriptBridge.m
@@ -53,6 +53,10 @@
     _base.messageHandlers[handlerName] = [handler copy];
 }
 
+- (void)removeHandler:(NSString *)handlerName {
+    [_base.messageHandlers removeObjectForKey:handlerName];
+}
+
 - (void)reset {
     [_base reset];
 }
