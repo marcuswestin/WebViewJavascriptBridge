a0cfcf6a10eee52834c8aa490f1b0a0bab9e7ca3
汤海波 <thb@luochen.com>
2017-09-20 17:43:03 +0800

    fix crash in iOS 11:  The app has encountered an unhandled exception: Completion handler passed to -[WKWebViewJavascriptBridge webView:decidePolicyForNavigationAction:decisionHandler:] was called more than once

--- WebViewJavascriptBridge/WKWebViewJavascriptBridge.m
+++ WebViewJavascriptBridge/WKWebViewJavascriptBridge.m
@@ -148,9 +148,7 @@ decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
             [_base logUnkownMessage:url];
         }
         decisionHandler(WKNavigationActionPolicyCancel);
-    }
-    
-    if (strongDelegate && [strongDelegate respondsToSelector:@selector(webView:decidePolicyForNavigationAction:decisionHandler:)]) {
+    } else if (strongDelegate && [strongDelegate respondsToSelector:@selector(webView:decidePolicyForNavigationAction:decisionHandler:)]) {
         [_webViewDelegate webView:webView decidePolicyForNavigationAction:navigationAction decisionHandler:decisionHandler];
     } else {
         decisionHandler(WKNavigationActionPolicyAllow);
