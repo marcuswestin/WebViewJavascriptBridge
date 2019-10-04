01b56425d07c2c91554aea0d1f3b31d83089b976
侯森魁 <senkui.hou@huifu.com>
2019-10-04 15:35:09 +0800

    Delete UIWebView API that Apple have fidden to use.

--- Example Apps/ExampleApp-iOS/ExampleAppDelegate.m
+++ Example Apps/ExampleApp-iOS/ExampleAppDelegate.m
@@ -1,25 +1,15 @@
 #import "ExampleAppDelegate.h"
-#import "ExampleUIWebViewController.h"
 #import "ExampleWKWebViewController.h"
 
 @implementation ExampleAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    // 1. Create the UIWebView example
-    ExampleUIWebViewController* UIWebViewExampleController = [[ExampleUIWebViewController alloc] init];
-    UIWebViewExampleController.tabBarItem.title             = @"UIWebView";
-
-    // 2. Create the tab footer and add the UIWebView example
     UITabBarController *tabBarController = [[UITabBarController alloc] init];
-    [tabBarController addChildViewController:UIWebViewExampleController];
     
-    // 3. Create the  WKWebView example for devices >= iOS 8
-    if([WKWebView class]) {
-        ExampleWKWebViewController* WKWebViewExampleController = [[ExampleWKWebViewController alloc] init];
-        WKWebViewExampleController.tabBarItem.title             = @"WKWebView";
-        [tabBarController addChildViewController:WKWebViewExampleController];
-    }
+    ExampleWKWebViewController* WKWebViewExampleController = [[ExampleWKWebViewController alloc] init];
+    WKWebViewExampleController.tabBarItem.title            = @"WKWebView";
+    [tabBarController addChildViewController:WKWebViewExampleController];
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.rootViewController = tabBarController;
